3a15c6ae38d351f4aed2ebded12ede6bde3afb47
Jonatã Oliveira <jonata.96ol@gmail.com>
2024-08-12 01:33:00 -0300

    feat: ✨ build script for react native

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,4 +1,16 @@
-# !/bin/sh
+#!/bin/zsh
 
+echo "===== Installling CocoaPods ====="
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 brew install cocoapods
+echo "===== Installing Node.js ====="
+brew install node@21
+echo "===== Installing yarn ====="
+brew install yarn
+
+# Install dependencies
+echo "===== Running yarn install ====="
+yarn install
+echo "===== Running pod install ====="
+cd ios
 pod install
